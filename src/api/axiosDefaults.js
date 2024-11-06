c23721a06ab80e958d0188458472ab94ffe0b4eb
Cheyenne David <ctenmanikdavid@gmail.com>
2024-11-06 06:02:37 +0000

    Fixed axios baseURL and Content-Type header for API requests

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "https://foraging-api-5bc654d11954.herokuapp.com/";
-axios.defaults.headers.post["Content-Type"] = "multipart/forma-data";
+axios.defaults.baseURL = "https://foraging-api-5bc654d11954.herokuapp.com";
+axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
