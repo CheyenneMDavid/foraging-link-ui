9827b2399ff5c10e1c2197c3a494ab01167149f4
Cheyenne David <ctenmanikdavid@gmail.com>
2024-11-20 23:37:25 +0000

    Added comments, aria-labels and re-ordered link attributes for consistency and clarity

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,81 +1,89 @@
-import React from "react";
-import { Navbar, Container, Nav } from "react-bootstrap";
-import logo from "../assets/logo.png";
-import styles from "../styles/NavBar.module.css";
-import { NavLink } from "react-router-dom";
-import {
-  useCurrentUser,
-  useSetCurrentUser,
-} from "../contexts/CurrentUserContext";
-import Avatar from "./Avatar";
-import axios from "axios";
-import useClickOutsideToggle from "../hooks/src/hooks/useClickOutsideToggle";
+import React from 'react';
+import { Navbar, Container, Nav } from 'react-bootstrap';
+import { NavLink } from 'react-router-dom';
+import axios from 'axios';
+import logo from '../assets/logo.png';
+import styles from '../styles/NavBar.module.css';
+import { useCurrentUser, useSetCurrentUser } from '../contexts/CurrentUserContext';
+import Avatar from './Avatar';
 
-const NavBar = () => {
+import useClickOutsideToggle from '../hooks/src/hooks/useClickOutsideToggle';
+
+// Navigation bar component, displays different navigation icons based on the user's
+// authentication status.
+function NavBar() {
   const currentUser = useCurrentUser();
-  const setCurrentUser = useSetCurrentUser();
+  const owner = currentUser?.username;
 
+  const setCurrentUser = useSetCurrentUser();
   const { expanded, setExpanded, ref } = useClickOutsideToggle();
 
+  // Handles logging out
   const handleSignOut = async () => {
     try {
-      await axios.post("dj-rest-auth/logout/");
+      await axios.post('dj-rest-auth/logout/');
       setCurrentUser(null);
     } catch (err) {
       console.log(err);
     }
   };
 
+  // Navigation view for users that are logged in
   const loggedInIcons = (
     <>
       <NavLink
         className={styles.NavLink}
+        activeClassName={styles.Active}
         to={`/profiles/${currentUser?.profile_id}`}
+        // Dynamic labeling displays actual user's name if it's available, otherwise it falls
+        // back on a description of "Profile of User"
+        aria-label={`Profile of ${owner || 'user'}`}
       >
-        <Avatar src={currentUser?.profile_image} text="Profile" height={50} />
+        <Avatar src={currentUser?.profile_image} text={owner} className={styles.NavAvatar} />
       </NavLink>
-      <NavLink className={styles.NavLink} to="/" onClick={handleSignOut}>
-        <i className="fa-solid fa-right-from-bracket"></i> Sign out
+
+      {/* Sign Out */}
+      <NavLink className={styles.NavLink} to="/" onClick={handleSignOut} aria-label="Sign out">
+        <i className="fa-solid fa-right-from-bracket" /> Sign out
       </NavLink>
     </>
   );
+
+  // Navigation view when users are logged out.
   const loggedOutIcons = (
     <>
       <NavLink
         className={styles.NavLink}
         activeClassName={styles.Active}
         to="/signin"
+        aria-label="Sign up"
       >
-        <i className="fas fa-sign-in-alt"></i> Sign in
+        <i className="fas fa-sign-in-alt" /> Sign in
       </NavLink>
       <NavLink
-        to="/signup"
         className={styles.NavLink}
         activeClassName={styles.Active}
+        to="/signup"
+        aria-label="Sign up"
       >
-        <i className="fas fa-user-plus"></i> Sign up
+        <i className="fas fa-user-plus" /> Sign up
       </NavLink>
     </>
   );
 
   return (
-    <Navbar
-      expanded={expanded}
-      className={styles.NavBar}
-      expand="md"
-      fixed="top"
-    >
+    <Navbar expanded={expanded} className={styles.NavBar} expand="md" fixed="top">
       <Container>
         <NavLink to="/">
           <Navbar.Brand>
             <img src={logo} alt="logo" height="55" />
           </Navbar.Brand>
         </NavLink>
-
         <Navbar.Toggle
           ref={ref}
           onClick={() => setExpanded(!expanded)}
           aria-controls="basic-navbar-nav"
+          aria-label="Toggle navigation"
         />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto text-left">
@@ -84,24 +92,24 @@ const NavBar = () => {
               className={styles.NavLink}
               activeClassName={styles.Active}
               to="/"
+              aria-label="Home"
             >
-              <i className="fas fa-home"></i> Home
+              <i className="fas fa-home" /> Home
             </NavLink>
-
             <NavLink
-              to="/courses"
               className={styles.NavLink}
               activeClassName={styles.Active}
+              to="/courses"
+              aria-label="Courses"
             >
-              <i className="fa-solid fa-location-arrow"></i> Courses
+              <i className="fa-solid fa-location-arrow" /> Courses
             </NavLink>
-
             {currentUser ? loggedInIcons : loggedOutIcons}
           </Nav>
         </Navbar.Collapse>
       </Container>
     </Navbar>
   );
-};
+}
 
 export default NavBar;
