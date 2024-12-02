76f3d9de204bcdac694d2e79758160ce9941a719
Cheyenne David <ctenmanikdavid@gmail.com>
2024-12-02 03:41:45 +0000

    Updated HomePage to include PostsList component and placeholder for the sidebar

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,19 +1,30 @@
-// Component that serves as the landing page, displaying the PostList and any associated Comments if they're available.
-
+// Component that serves as the landing page, displaying the PostsList and Sidebar
 import React from "react";
-import styles from "../styles/Post.module.css";
-import PostList from "../components/PostList";
 
-const HomePage = () => {
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+
+import PostsList from "./posts/PostsList";
+import { useCurrentUser } from "../contexts/CurrentUserContext";
+
+function HomePage() {
+  const currentUser = useCurrentUser();
+  const profile_id = currentUser?.profile_id || "";
+
   return (
-    <div>
-      <h1 className={styles.TempColor1}>
-        HomePage: Displays the PostList after pulling in content from
-        PostList.js
-      </h1>
-      <PostList />
-    </div>
+    <Row className="h-100">
+      {/* Main Content */}
+      <Col lg={8} className="py-2 p-0 p-lg-2">
+        <PostsList message="No results found. Adjust the search keyword." />
+      </Col>
+
+      {/* Sidebar */}
+      <Col md={4} className="d-none d-lg-block p-0 p-lg-2">
+        <p>Courses</p>
+        <p>Most Followed</p>
+      </Col>
+    </Row>
   );
-};
+}
 
 export default HomePage;
