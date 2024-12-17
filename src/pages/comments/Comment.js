34131161cb43707359c68dda13ab015eb4474e3b
Cheyenne David <ctenmanikdavid@gmail.com>
2024-12-17 14:14:11 +0000

    Added structure and initial styling for comment replies with CommentBar components

--- src/pages/comments/Comment.js
+++ src/pages/comments/Comment.js
@@ -1,21 +1,38 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
 import Avatar from "../../components/Avatar";
 import styles from "../../styles/Comment.module.css";
 
 function Comment(props) {
-  const { owner, profile_image, created_at, content, is_owner } = props;
+  const {
+    owner,
+    profile_image,
+    profile_id,
+    created_at,
+    content,
+    likes_count,
+    isReply,
+  } = props;
 
   return (
-    <div className={styles.Comment}>
-      <div className="d-flex align-items-center">
-        {/* Avatar and user info */}
-        <Avatar src={profile_image} height={30} />
-        <div className="ml-2">
+    // Conditionally applies styles based on whether the comment is a top-level comment or a reply with a parent.
+    <div className={isReply ? styles.Reply : styles.Comment}>
+      {/* Aligns the stacked avatar and owner's name side by side with the content. */}
+      <div className={styles.CommentContainer}>
+        <div className={styles.CommentAvatar}>
+          <Link to={`/profiles/${profile_id}`}>
+            <Avatar src={profile_image} height={40} alt={`${owner}'s avatar`} />
+          </Link>
           <strong>{owner}</strong>
-          <span className="text-muted ml-2">{created_at}</span>
         </div>
+        <p>{content}</p>
       </div>
-      <p>{content}</p>
+      <div className={styles.CommentBar}>
+        <span>{created_at}</span>
+        <i className="far fa-heart"></i>
+        {likes_count > 0 && <span>{likes_count}</span>}
+      </div>
     </div>
   );
 }
