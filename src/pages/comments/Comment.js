cc5f4829fee8359545dd8ab6d4f7baafc972e1bd
Cheyenne David <ctenmanikdavid@gmail.com>
2024-12-18 10:53:25 +0000

    Updated Comment component for more semantic structure

--- src/pages/comments/Comment.js
+++ src/pages/comments/Comment.js
@@ -1,6 +1,6 @@
 import React from "react";
+import { Card } from "react-bootstrap";
 import { Link } from "react-router-dom";
-
 import Avatar from "../../components/Avatar";
 import styles from "../../styles/Comment.module.css";
 
@@ -16,24 +16,41 @@ function Comment(props) {
   } = props;
 
   return (
-    // Conditionally applies styles based on whether the comment is a top-level comment or a reply with a parent.
-    <div className={isReply ? styles.Reply : styles.Comment}>
-      {/* Aligns the stacked avatar and owner's name side by side with the content. */}
-      <div className={styles.CommentContainer}>
-        <div className={styles.CommentAvatar}>
-          <Link to={`/profiles/${profile_id}`}>
-            <Avatar src={profile_image} height={40} alt={`${owner}'s avatar`} />
-          </Link>
-          <strong>{owner}</strong>
+    // Conditionally applies styles based on whether the comment is a top-level comment or a
+    // reply with a parent.
+    <Card className={isReply ? styles.Reply : styles.Comment}>
+      <Card.Body>
+        <div className={styles.CommentContainer}>
+          {/* Avatar and Username stacks to left, making most of space */}
+          <div className={styles.CommentHead}>
+            <Link to={`/profiles/${profile_id}`}>
+              <Avatar
+                src={profile_image}
+                height={40}
+                alt={`${owner}'s avatar`}
+              />
+            </Link>
+            <strong className={styles.OwnerName}>{owner}</strong>
+          </div>
+
+          {/* Comment Content to the right of the stacked Avatar and username */}
+          <div className={styles.CommentContent}>
+            <Card.Text>{content}</Card.Text>
+          </div>
         </div>
-        <p>{content}</p>
-      </div>
-      <div className={styles.CommentBar}>
-        <span>{created_at}</span>
-        <i className="far fa-heart"></i>
-        {likes_count > 0 && <span>{likes_count}</span>}
-      </div>
-    </div>
+
+        {/* User Interaction section */}
+        <div className={styles.CommentBar}>
+          <span>{created_at}</span>
+          <i className="far fa-heart"></i>
+          {likes_count > 0 && <span>{likes_count}</span>}
+
+          {/* Condisional display of edit and trash dependant on currently logged in user being the owner of comment */}
+          <p>Edit Button</p>
+          <p>Trash Can</p>
+        </div>
+      </Card.Body>
+    </Card>
   );
 }
 
