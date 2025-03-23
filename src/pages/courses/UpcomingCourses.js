fbfbe7887b8dd6a5fb9fa6afc0a22eb6c6991932
CheyenneMDavid <ctenmanikdavid@gmail.com>
2025-03-23 16:51:11 +0000

    Added UpcomingCourses.js

---
+++ src/pages/courses/UpcomingCourses.js
@@ -0,0 +1,45 @@
+// Component to display the 3 most immediate upcoming courses, used in the sidebar
+import React, { useEffect, useState } from "react";
+import { axiosReq } from "../../api/axiosDefaults";
+
+import styles from "../../styles/UpcomingCourses.module.css";
+
+function UpcomingCourses() {
+  // State to store the list of upcoming courses.
+  const [courses, setCourses] = useState([]);
+
+  // Fetches the upcoming courses when the component mounts
+  useEffect(() => {
+    const fetchCourses = async () => {
+      try {
+        // Send GET request to the API endpoint for upcoming courses
+        const { data } = await axiosReq.get("/courses/");
+        console.log("Fetched data:", data);
+        setCourses(data.results);
+      } catch (err) {
+        console.log("Error fetching upcoming courses:", err);
+      }
+    };
+
+    fetchCourses();
+  }, []);
+
+  // Renders the
+  return (
+    <div>
+      <h3>Upcoming Courses</h3>
+      <ul>
+        {courses.map((course) => (
+          <li key={course.id}>
+            <h5 className={styles.CourseTitle}>{course.title}</h5>
+            {/* Formats the course date into a more readable format */}
+            <p>{new Date(course.date).toLocaleDateString()}</p>
+            <hr />
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default UpcomingCourses;
