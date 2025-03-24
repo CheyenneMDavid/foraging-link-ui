fb29b3ccef41ac902a2b25cbb205f98a7189e1e2
CheyenneMDavid <ctenmanikdavid@gmail.com>
2025-03-24 12:43:46 +0000

    fixed import route for UpcomingCourses.js

---
+++ src/components/Searchbar.js
@@ -0,0 +1,33 @@
+// SearchBar.js
+// THIS IS A TEMPORARY WORKINGS FOR USING THE SEARCHBAR AS A REUSABLE COMPONENT.
+import React, { useState, useEffect } from "react";
+import Form from "react-bootstrap/Form";
+import styles from "../styles/SearchBar.module.css";
+
+const SearchBar = ({ setQuery }) => {
+  const [search, setSearch] = useState("");
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setQuery(search);
+    }, 1000);
+    return () => clearTimeout(timer);
+  }, [search, setQuery]);
+
+  return (
+    <Form
+      className={styles.SearchBar}
+      onSubmit={(event) => event.preventDefault()}
+    >
+      <i className={`fas fa-search ${styles.SearchIcon}`} />
+      <Form.Control
+        value={search}
+        onChange={(event) => setSearch(event.target.value)}
+        type="text"
+        placeholder="Search"
+      />
+    </Form>
+  );
+};
+
+export default SearchBar;
