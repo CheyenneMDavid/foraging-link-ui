4847240a790919d353a7b552be8985fb0578dd98
CheyenneMDavid <ctenmanikdavid@gmail.com>
2025-05-16 20:39:06 +0100

    Added Course.js with comments, purpose of file

---
+++ src/pages/courses/Course.js
@@ -0,0 +1,3 @@
+// Purpose of Course.js is to render a single course's data using props passed to it.
+// It's used to display course information on the detail pages,
+// the course list pages, and the upcoming courses in the sidebar.
