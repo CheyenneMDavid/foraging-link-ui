97301f05497b68411487a3a58b1949705c52109f
CheyenneMDavid <ctenmanikdavid@gmail.com>
2025-06-30 19:30:06 +0100

    Created Course.js to render full course details

--- src/pages/courses/Course.js
+++ src/pages/courses/Course.js
@@ -1,3 +1,30 @@
-// Purpose of Course.js is to render a single course's data using props passed to it.
-// It's used to display course information on the detail pages,
-// the course list pages, and the upcoming courses in the sidebar.
+// The purpose of Course.js is to render the full course details.
+// It is used in CoursePage.js and CoursesListPage.js.
+// Description is shown in full, is truncated or discluded depending on context.
+
+import React from "react";
+
+function Course({ course }) {
+  const {
+    id,
+    season,
+    title,
+    date,
+    description,
+    location,
+    max_capacity,
+    available_spaces,
+  } = course;
+
+  return (
+    <div>
+      <h4>{title}</h4>
+      <p>Date: {new Date(date).toLocaleDateString()}</p>
+      <p>Season: {season}</p>
+      <p>Location: {location}</p>
+      <p>Description: {description}</p>
+      <p>Spaces Available: {available_spaces}</p>
+    </div>
+  );
+}
+export default Course;
