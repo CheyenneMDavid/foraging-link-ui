277a21cb44d80a18f9a27cc96f3aeb4d82f41b40
CheyenneMDavid <ctenmanikdavid@gmail.com>
2025-07-06 22:10:41 +0100

    Simplified Course.js and updated structure to match posts

--- src/pages/courses/Course.js
+++ src/pages/courses/Course.js
@@ -1,30 +1,21 @@
-// The purpose of Course.js is to render the full course details.
-// It is used in CoursePage.js and CoursesListPage.js.
-// Description is shown in full, is truncated or discluded depending on context.
+// Course.js displays course data using props.
 
 import React from "react";
+import { Card } from "react-bootstrap";
 
-function Course({ course }) {
-  const {
-    id,
-    season,
-    title,
-    date,
-    description,
-    location,
-    max_capacity,
-    available_spaces,
-  } = course;
+function Course(props) {
+  const { season, title, date, description, location, available_spaces } =
+    props;
 
   return (
-    <div>
+    <Card.Body>
       <h4>{title}</h4>
       <p>Date: {new Date(date).toLocaleDateString()}</p>
       <p>Season: {season}</p>
       <p>Location: {location}</p>
       <p>Description: {description}</p>
       <p>Spaces Available: {available_spaces}</p>
-    </div>
+    </Card.Body>
   );
 }
 export default Course;
