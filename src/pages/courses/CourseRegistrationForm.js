1185f45d7f41283884f2a9dbd0df7f2c65c719bf
CheyenneMDavid <ctenmanikdavid@gmail.com>
2025-07-08 16:24:22 +0100

    Added state variables for name, email and phonenumber

--- src/pages/courses/CourseRegistrationForm.js
+++ src/pages/courses/CourseRegistrationForm.js
@@ -1,6 +1,12 @@
-import React from "react";
+// This form collects and submits user details to register for a course.
 
-const CourseRegistrationForm = () => {
+import React, { useState } from "react";
+
+const CourseRegistrationForm = ({ courseId }) => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+
   return <div>CourseRegistrationForm</div>;
 };
 
