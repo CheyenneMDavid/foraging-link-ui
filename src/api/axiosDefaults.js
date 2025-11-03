fa2d1f913ff47f0c495aeb11e2493576fdbe5c2a
CheyenneMDavid <ctenmanikdavid@gmail.com>
2025-11-03 16:45:36 +0000

    Added trailing slash to API base URL

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "https://foraging-api-b287953c9098.herokuapp.com";
+axios.defaults.baseURL = "https://foraging-api-b287953c9098.herokuapp.com/";
 
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
