41b0b02653c673a267ea7af5835533c3f12e4f4a
CheyenneMDavid <ctenmanikdavid@gmail.com>
2025-11-06 19:01:55 +0000

    Added Cloudinary auto-format and size optimization to Avatar

--- src/components/Avatar.js
+++ src/components/Avatar.js
@@ -1,14 +1,21 @@
-import React from 'react';
-import styles from '../styles/Avatar.module.css';
+import React from "react";
+import styles from "../styles/Avatar.module.css";
 
-// Displays a user's avatar with styling applied dynamically, allowing avatars to be styled
-// differently based on where they are used.
+const resizeAvatar = (url, width, height) =>
+  url && url.includes("res.cloudinary.com")
+    ? url.replace(
+        "/upload/",
 
-function Avatar({ src, alt = 'avatar', text, height = 40, width = 40 }) {
+        `/upload/f_auto,q_auto,w_${width},h_${height},c_fill,g_auto/`
+      )
+    : url;
+
+function Avatar({ src, alt = "avatar", text, height = 50, width = 50 }) {
+  const formattedSrc = resizeAvatar(src, width, height);
   return (
     <span>
       <img
-        src={src}
+        src={formattedSrc}
         alt={alt}
         className={styles.Avatar}
         style={{ height: `${height}px`, width: `${width}px` }}
